45c93409722845a33ebdc75a1db8911cc1c5f7bb
2apoorva5 <cu.16bcs1256@gmail.com>
2020-07-07 01:55:29 +0530

    Seventh Commit

--- app/src/main/java/com/developerdepository/noted/MainActivity.java
+++ app/src/main/java/com/developerdepository/noted/MainActivity.java
@@ -10,7 +10,6 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.provider.Settings;
